4331610e23b1a010b93922bca56895e973ea26fa
Nikki Robinson <nikkirobinsonphoto@gmail.com>
2016-02-11 20:37:18 -0500

    5.3 Sending emails, added email form to contact page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,14 @@ class StaticPagesController < ApplicationController
   	def index
   	end
 
+  	def thank_you
+  		@name = params[:name]
+  		@email = params[:email]
+  		@message = params[:message]
+  		ActionMailer::Base.mail(:from => @email,
+  			:to => 'nikkirobinsonphotography@gmail.com',
+  			:subject => "A new contact form message from #{@name}",
+  			:body => @message).deliver_now
+  	end
+
 end
