b6ace55b161760ecf528bb14a6ddc8ecdcc9dfbe
Nikki Robinson <nikkirobinsonphoto@gmail.com>
2016-03-12 20:16:24 -0500

    cleaning up code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,11 @@ class UserMailer < ApplicationMailer
 			:subject => "A new contact form message from #{name}")
 	end
 
+	def welcome(user)
+		@appname = "Nikki's Photos"
+		mail( :to => user.email,
+			:subject => "Welcome to #{@appname}!")
+
 	def thank_you
 		@name = params[:name]
 		@email = params[:email]
