3a011fabc1812c725a5492bb93eb8e3a9b38a5d3
Nikki Robinson <nikkirobinsonphoto@gmail.com>
2016-03-30 19:36:02 -0400

    fixed some code

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,9 +4,9 @@ $(document).on('ready page:load', function() {
 	angular.bootstrap(document.body, ['shop'])
 });
 
-app.factory('models', [function($resource){
+app.factory('models', ['$resource', function($resource){
 	var orders_model = $resource("/orders/:id.json", {id: "@id"});
-  	var products_model = $resource("/products/:id.json", {id: "@id"});
+	var products_model = $resource("/products/:id.json", {id: "@id"});
 	var x = {
 		orders: orders_model,
 		products: products_model
@@ -18,16 +18,17 @@ app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
 	$scope.orders = models.orders.query();
 	$scope.products = models.orders.query();
 
-  	$scope.addOrder = function(){
-  		if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
-  		order = models.orders.save($scope.newOrder, function(){
-  		recent_order = models.orders.get({id: order.id});
-  		$scope.orders.push(recent_order);
-  		$scope.newOrder = '';
-	});
-  	
-  	$scope.deleteOrder = function(order){
-  		models.orders.delete(order);
-  		$scope.orders.splice($scope.orders.indexOf(order), 1);
+	$scope.addOrder = function(){
+		if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
+		order = models.orders.save($scope.newOrder, function(){
+			recent_order = models.orders.get({id: order.id});
+			$scope.orders.push(recent_order);
+			$scope.newOrder = '';
+		});
 	}
-}]);
+
+		$scope.deleteOrder = function(order){
+			models.orders.delete(order);
+			$scope.orders.splice($scope.orders.indexOf(order), 1);
+		}
+	}]);
